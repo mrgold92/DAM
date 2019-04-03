b6c60513b961eda0367022d9d60599c307d4c3de
David Salazar <salazarrodero.david@gmail.com>
2019-04-03 20:52:11 +0200

    nuevo

---
+++ DAM/src/tema6/practica4/Principal.java
@@ -0,0 +1,60 @@
+package tema6.practica4;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class Principal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public void crearVentana() {
+
+		setTitle("Primera ventana");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(700, 650);
+		setLocationRelativeTo(null);
+		//setExtendedState(JFrame.MAXIMIZED_HORIZ);
+		setResizable(false);
+		changeIcon();
+		setVisible(true);
+
+	}
+
+	public void otraVentana() {
+
+		setTitle("Segunda ventana");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		centrar();
+		changeIcon();
+		setVisible(true);
+	}
+
+	public void centrar() {
+
+		Toolkit pantalla = Toolkit.getDefaultToolkit();
+		Dimension tpantalla = pantalla.getScreenSize();
+		int altura = tpantalla.height;
+		int ancho = tpantalla.width;
+		setSize(ancho, altura / 2);
+		setLocation(ancho / altura, altura / 4);
+
+	}
+
+	public void changeIcon() {
+		Toolkit pantalla = Toolkit.getDefaultToolkit();
+		Image icono = pantalla.getImage("resources/img-icon-interface.png");
+		setIconImage(icono);
+	}
+
+	public static void main(String[] args) {
+
+		Principal ventana = new Principal();
+
+		ventana.crearVentana();
+		ventana.otraVentana();
+
+	}
+
+}
