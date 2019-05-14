a9046028f9e54107737e9c0a6201abcf00a7354d
David <davids1_4@hotmail.com>
2019-05-14 07:05:31 +0200

    Practica 1 tema 8 actualizando conexion

--- DAM/src/tema8/practica1/Conexion.java
+++ DAM/src/tema8/practica1/Conexion.java
@@ -23,7 +23,7 @@ public class Conexion {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda?useSSL=true&requireSSL=false", "root", "");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda", "root", "");
 			sta = conn.createStatement();
 
 		} catch (SQLException e) {
